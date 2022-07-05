83f8ee8bf2e39b30241a74a28017822c75c7498d
PlasticBucket1944 <Orangina2739@gmail.com>
2022-07-05 11:01:06 +0900

    configファイルから設定を読み取れるように変更

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ConfigService } from '@nestjs/config';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private configService: ConfigService,
+    ) {}
 
   @Get()
   getHello(): string {
+    console.log(process.env.NODE_ENV);
+    console.log(this.configService.get<string>('HOGE'));
+    console.log(this.configService.get<string>('TEST'));
     return this.appService.getHello();
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  // 環境変数を確認
+  //export NODE_ENV='development' or 'production'
+  switch (process.env.NODE_ENV) {
+    case 'development':
+    case 'production':
+      break;
+    default:
+      throw Error('環境変数が設定されていません。 development or production');
+  }
+
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
