d9a34c9bacfbc0eb5397935cddd1a9236ac5fd2c
PlasticBucket1944 <Orangina2739@gmail.com>
2022-07-06 19:49:24 +0900

    typeormの設定を追加しDBに接続できるように変更

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    envFilePath: [`env/${process.env.NODE_ENV}.env`],
-})],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`env/${process.env.NODE_ENV}.env`],
+    }),
+    TypeOrmModule.forRoot(),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
