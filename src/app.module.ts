3493bf7af805b061b81c8998f4672ae14be64849
PlasticBucket1944 <Orangina2739@gmail.com>
2022-07-08 07:42:16 +0900

    nest g module tasks 実行

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       envFilePath: [`env/${process.env.NODE_ENV}.env`],
     }),
     TypeOrmModule.forRoot(),
+    TasksModule,
   ],
   controllers: [AppController],
   providers: [AppService],
