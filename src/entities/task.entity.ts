f52aa7e3188e08679d2d918d707d63985e1926e8
PlasticBucket1944 <Orangina2739@gmail.com>
2022-07-09 22:10:10 +0900

    タスクエンティティファイル追加

---
+++ src/entities/task.entity.ts
@@ -0,0 +1,19 @@
+import { TaskStauts } from "src/tasks/task-status.enum";
+import { Column, PrimaryGeneratedColumn } from "typeorm";
+
+export class Task {
+  @PrimaryGeneratedColumn('uuid', { comment: 'ID' })
+  id:string;
+
+  @Column({ comment: 'タスク名' })
+  name: string;
+
+  @Column({ comment: 'タスク状態 1:未実行、2:実行中、3:完了' })
+  stasus: TaskStauts;
+
+  @Column({ comment: '更新ログ項目：作成日' })
+  createdAt: string;
+
+  @Column({ comment: '更新ログ項目：更新日' })
+  updatedAt: string;
+}
