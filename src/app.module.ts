10b7306aba7cb0e98fd288be13c023c5a4e375dc
PlasticBucket1944 <Orangina2739@gmail.com>
2022-09-26 18:56:32 +0900

    DBG

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,23 @@ import { TasksModule } from './tasks/tasks.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: [`env/${process.env.NODE_ENV}.env`],
+      // envFilePath: [`env/${process.env.NODE_ENV}.env`],
     }),
-    TypeOrmModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      autoLoadEntities: true,
+      entities: ['dist/entities/*.entity.js'],
+      migrations: ['dist/migrations/*.js'],
+      cli: {
+          entitiesDir: 'src/entities',
+          migrationsDir: 'src/migrations',
+      },
+    }),
     TasksModule,
   ],
   controllers: [],
