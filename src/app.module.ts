63842aa156ed446c446189d24df1196617afdd2a
PlasticBucket1944 <Orangina2739@gmail.com>
2022-09-26 19:17:00 +0900

    DBG

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      // envFilePath: [`env/${process.env.NODE_ENV}.env`],
-    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
