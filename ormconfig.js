6e2b12b2074bb3a67da089a85940261fd12ebc36
PlasticBucket1944 <Orangina2739@gmail.com>
2022-09-27 00:42:35 +0900

    test

---
+++ ormconfig.js
@@ -0,0 +1,16 @@
+module.exports = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  autoLoadEntities: true,
+  // entities: ['dist/entities/*.entity.js'],
+  entities: [__dirname + '/entities/*.js'],
+  migrations: ['dist/migrations/*.js'],
+  cli: {
+      entitiesDir: 'src/entities',
+      migrationsDir: 'src/migrations',
+  },
+};
