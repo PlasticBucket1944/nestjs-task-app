c26e3e4b90358c014b765ffb68195ae5f92b6b80
PlasticBucket1944 <Orangina2739@gmail.com>
2022-09-27 13:57:17 +0900

    ポート番号を動的に受け取るように変更

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,7 @@ async function bootstrap() {
     allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
   });
 
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
